5e95ef6bea78fdf36f0ea428f9a4a4f5bc45a3d9
drake-error <darwin2010p@gmail.com>
2025-09-08 18:54:20 +0530

    Initial commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String args[])
+    {
+        int container = 67;
+        String SHRUTHI = "DARWIN";
+        String container2 = SHRUTHI;
+        System.out.print(container);
+        System.out.print(container2);
+    }
+}
