4a09b080181a4fddcb6aee57622531ae7af7628e
drake-error <darwin2010p@gmail.com>
2025-09-08 19:18:58 +0530

    Modified hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ public class hello {
     public static void main(String args[])
     {
         int container = 67;
-        String SHRUTHI = "DARWIN";
-        String container2 = SHRUTHI;
+        String AFFAN = "DARWIN";
+        String container2 = AFFAN;
         System.out.print(container);
         System.out.print(container2);
     }
