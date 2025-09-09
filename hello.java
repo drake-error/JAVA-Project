cf001efc59ac4e7f7f8493f5eefabf4dd85c4fa3
drake-error <darwin2010p@gmail.com>
2025-09-09 16:27:32 +0530

    Modified hello.java

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 public class hello {
     public static void main(String args[])
     {
-        int container = 67;
-        String AFFAN = "DARWIN";
-        String container2 = AFFAN;
-        System.out.print(container);
-        System.out.print(container2);
+        System.out.print("Hello World");
     }
 }
