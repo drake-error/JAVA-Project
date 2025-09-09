473def17dd5f64266d81abbe0500634e2944b7e5
drake-error <darwin2010p@gmail.com>
2025-09-09 16:14:24 +0530

    New File variableanddatatype.java created

---
+++ variableanddatatype.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String args[])
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = a+b+c;
+        System.out.print(d);
+    }
+}
