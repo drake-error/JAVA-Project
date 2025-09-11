6c17b927e8b2b01d09a23f4032c0fc99bed158e7
drake-error <darwin2010p@gmail.com>
2025-09-11 22:02:21 +0530

    double

--- q2.java
+++ q2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class q2 {
     public static void main(String args[])
     {
-        float b = 5.678567f;
-        System.out.print("precision up to 6 decimal places ");
+        double b = 5.678567867;
+        System.out.print("stores many decimals , large size");
     }
 }
