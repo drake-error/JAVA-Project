20c367f07b403d746a438372f886429537165af2
drake-error <darwin2010p@gmail.com>
2025-09-11 21:53:13 +0530

    learnt byte short int long float double char

--- q2.java
+++ q2.java
@@ -3,12 +3,7 @@ import java.util.*;
 class q2 {
     public static void main(String args[])
     {
-        Scanner scan = new Scanner(System.in);
-        String name = scan.nextLine();
-        double marks = scan.nextDouble();
-        String department = scan.next(); 
-        System.out.print("My Name is "+name);
-        System.out.print("\nMy Marks is "+marks/10 +"/10");
-        System.out.print("\nMy Department is "+department);
+        String name = "Hello";
+        System.out.print(name.charAt(0));
     }
 }
