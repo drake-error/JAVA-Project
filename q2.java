561daa1302a8523aa09a2aadbde67e3711ca053c
drake-error <darwin2010p@gmail.com>
2025-09-11 21:56:49 +0530

    Byte

--- q2.java
+++ q2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class q2 {
     public static void main(String args[])
     {
-        String name = "Hello";
-        System.out.print(name.charAt(0));
+        byte b = 5;
+        System.out.print("Stores in less memory");
     }
 }
