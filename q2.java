df80d3170a709d844258b5278b5fdfc166fe9c8d
drake-error <darwin2010p@gmail.com>
2025-09-11 21:58:21 +0530

    short

--- q2.java
+++ q2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class q2 {
     public static void main(String args[])
     {
-        byte b = 5;
-        System.out.print("Stores in less memory");
+        short b = 5;
+        System.out.print("Stores less than 32k numbers , less memory");
     }
 }
