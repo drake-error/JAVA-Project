0dbfaa5b24849ca90a0abcc8250fbbb15b3be7f3
drake-error <darwin2010p@gmail.com>
2025-09-11 21:59:42 +0530

    long

--- q2.java
+++ q2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class q2 {
     public static void main(String args[])
     {
-        short b = 5;
-        System.out.print("Stores less than 32k numbers , less memory");
+        long b = 5;
+        System.out.print("Stores less than 2^31 numbers , large memory than int");
     }
 }
