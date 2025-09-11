f63655442058703b02c49b6e69a3bd96b166073a
drake-error <darwin2010p@gmail.com>
2025-09-11 22:00:47 +0530

    long

--- q2.java
+++ q2.java
@@ -3,7 +3,7 @@ import java.util.*;
 class q2 {
     public static void main(String args[])
     {
-        long b = 5;
-        System.out.print("Stores less than 2^31 numbers , large memory than int");
+        float b = 5.678567f;
+        System.out.print("precision up to 6 decimal places ");
     }
 }
