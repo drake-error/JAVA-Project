5ad5d93268200a0b3fd9e31ac0181b85b81eb7a3
drake-error <darwin2010p@gmail.com>
2025-09-12 20:31:05 +0530

    Modified ifelse.java

--- ifelse.java
+++ ifelse.java
@@ -1,5 +1,16 @@
 class ifelse {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        //scenario 1: it's going to rain
+        //scenario 2: no rain
+        
+        //System.out.println("Take an umbrella");
+
+        //System.out.print("Enjoy the sunlight");
+
+        if(true)
+        {
+            System.out.println("Yes");
+        }
     }
 }
