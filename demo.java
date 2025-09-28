5c2b9ac113e98ffe55b89ee7378f4aa0c31d5f31
drake-error <darwin2010p@gmail.com>
2025-09-28 18:50:31 +0530

    comparison operators

--- demo.java
+++ demo.java
@@ -2,20 +2,10 @@ import java.util.Scanner;
 class demo {
     public static void main(String[] args) 
     {
-        //Check whether Number 1 and Number 2 are equal or not
+        String f1 = "apple";
+        String f2 = "apple";
 
-        Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
-
-        if(num1 == num2)
-        {
-            System.out.println("Both the numbers are equal");
-        }
-        else
-        {
-            System.out.println("Not equal");
-        }
+        System.out.println(f1==f2);
         
     }
     
