743c68fc4ba4f4f92d204feff61bc2bf36350bc2
drake-error <darwin2010p@gmail.com>
2025-09-28 18:52:45 +0530

    modified

--- demo.java
+++ demo.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 class demo {
     public static void main(String[] args) 
     {
-        String f1 = "apple";
-        String f2 = "apple";
+        String f1 = new String ("apple");
+        String f2 = new String ("apple");
 
         System.out.println(f1==f2);
         
