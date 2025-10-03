a3626ff6621133da32ca86e7dc7d0ed9c67fa5a2
drake-error <darwin2010p@gmail.com>
2025-10-03 18:28:52 +0530

    New file added

---
+++ hi.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class hi {
+    public static void main(String args[])
+    {
+        // Create a Variable called num
+        // Find out whether num is divisible by 3 or not
+        // Find out whether num is divisible by 5 or not
+        // Divisible by 3 && Divisible by 5 
+
+        int num = 15;
+        if(num%3==0)
+        {
+            System.out.print("Yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+        if(num%5==0)
+        {
+            System.out.print("Yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+
+}
