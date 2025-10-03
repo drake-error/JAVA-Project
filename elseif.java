0aa5b5d44d5aa87902f55041f90454953861cfc6
drake-error <darwin2010p@gmail.com>
2025-10-03 22:44:37 +0530

    new file elseif

---
+++ elseif.java
@@ -0,0 +1,23 @@
+public class elseif {
+    public static void main(String args[])
+    {
+        int score = 70;
+
+        if (score>35 && score<60)
+        {
+            System.out.print("Video Game");
+        }
+        else if(score>60 && score<90)
+        {
+            System.out.print("Iphone");
+        }
+        else if(score>90)
+        {
+            System.out.print("Macbook");
+        }
+        
+      
+    }
+    
+
+}
