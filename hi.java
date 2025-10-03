ff46535ce82f1ca645e03cecf05906f9e99e86a3
drake-error <darwin2010p@gmail.com>
2025-10-03 18:33:03 +0530

    Modified

--- hi.java
+++ hi.java
@@ -4,27 +4,19 @@ class hi {
     public static void main(String args[])
     {
         // Create a Variable called num
-        // Find out whether num is divisible by 3 or not
-        // Find out whether num is divisible by 5 or not
-        // Divisible by 3 && Divisible by 5 
+        // Find out whether num is divisible by 2 or not
 
-        int num = 15;
-        if(num%3==0)
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if(num%2==0)
         {
-            System.out.print("Yes");
+            System.out.print("Even");
         }
         else
         {
-            System.out.print("no");
+            System.out.print("Odd");
         }
-        if(num%5==0)
-        {
-            System.out.print("Yes");
-        }
-        else
-        {
-            System.out.print("no");
-        }
+        
     }
 
 }
