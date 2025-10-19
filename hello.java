440da32aeab89817d6f8f3fed32f45feae42093d
drake-error <darwin2010p@gmail.com>
2025-10-19 15:44:08 +0530

    Operator Q

--- hello.java
+++ hello.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 class hello {
     public static void main(String args[])
     {
-        Scanner Darwin = new Scanner(System.in);
-        String b = Darwin.nextLine();
-        System.out.print(b);
+        Scanner scan = new Scanner(System.in);
+        int salary = scan.nextInt();
+        int age = scan.nextInt();
+
+        if(salary>20000 || age<25)
+        {
+            System.out.println("Eligible for loan");
+            int loan = scan.nextInt();
+
+            if(loan<500000)
+            {
+                System.out.println("Loan Available");
+            }
+            else
+            {
+                System.out.println("Loan Not Available");
+            }
+        }
+        else
+        {
+            System.out.println("No loan");
+        }
     }
 }
