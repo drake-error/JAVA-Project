a2c98af876088a9319340d9250f938d9e1956a90
drake-error <darwin2010p@gmail.com>
2025-10-19 16:41:23 +0530

    FOR LOOP Q

--- ifelse.java
+++ ifelse.java
@@ -1,21 +1,9 @@
 class ifelse {
     public static void main(String[] args) {
-        
-        //scenario 1: it's going to rain
-        //scenario 2: no rain
-        
-        //System.out.println("Take an umbrella");
-
-        //System.out.print("Enjoy the sunlight");
-
-        boolean rain = false;
-        if (rain)
-        {
-            System.out.println("Take an umbrella");
-        }
-        else
-        {
-            System.out.println("Enjoy the sunlight");
-        }
+       for(int i=1; i<=10; i=i+1)
+       {
+        System.out.println(i);
+       }
+          
     }
 }
