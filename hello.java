c729249e1b77db6bc296058d5ecc498f6fe85c22
drake-error <darwin2010p@gmail.com>
2025-10-19 15:51:21 +0530

    Operator q

--- hello.java
+++ hello.java
@@ -5,15 +5,19 @@ class hello {
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Your Salary:");
         int salary = scan.nextInt();
+        System.out.println("Enter Your Age:");
         int age = scan.nextInt();
 
-        if(salary>20000 || age<25)
+        if(salary>=20000 || age<25)
         {
             System.out.println("Eligible for loan");
+            System.out.println("Enter Loan Amount:");
             int loan = scan.nextInt();
 
-            if(loan<500000)
+            if(loan<50000)
             {
                 System.out.println("Loan Available");
             }
