ae286f2a4964066c9ff72c1d58b4010aeed2c8f6
drake-error <darwin2010p@gmail.com>
2025-10-20 21:27:31 +0530

    array q

--- ifelse.java
+++ ifelse.java
@@ -1,20 +1,17 @@
 class ifelse {
     public static void main(String[] args) {
-        int oddcount =0;
+         int evencount=0;
        for(int i=1; i<=10; i=i+1)
        {
         // print even numbers only
         if(i%2==0)
-        { 
-         // System.out.println("Even Number:"+i);
-        }
-        else
         {
-            oddcount = oddcount+1;
-            //System.out.println("Odd Number:"+i);
-            System.out.println(oddcount);
+            evencount = evencount+1; 
         }
        }
+
+       System.out.println(evencount);
+
           
     }
 }
