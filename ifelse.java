7c6f8305ccd9280b7008e5201725ac9202f8bd3d
drake-error <darwin2010p@gmail.com>
2025-10-20 20:04:38 +0530

    For Loop Q

--- ifelse.java
+++ ifelse.java
@@ -2,7 +2,15 @@ class ifelse {
     public static void main(String[] args) {
        for(int i=1; i<=10; i=i+1)
        {
-        System.out.println(i);
+        // print even numbers only
+        if(i%2==0)
+        { 
+         // System.out.println("Even Number:"+i);
+        }
+        else
+        {
+            System.out.println("Odd Number:"+i);
+        }
        }
           
     }
