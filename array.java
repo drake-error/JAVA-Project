60157f1dca29983768c92d9adf1a77cb9259c58b
drake-error <darwin2010p@gmail.com>
2025-10-21 21:37:57 +0530

    size array

--- array.java
+++ array.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 class array {
     public static void main(String args[])
     { 
-        for (int i=1; i<=10; i=i+1)
-        { 
-            System.out.println(i+"x2="+i*2);
+        //Get input for size of an array
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] marks = new int[size];
+
+        for (int i = 0; i <= size-1; i=i+1)
+        {
+            marks[i] = scan.nextInt();
         }
-        
     }
 }
