0d3a1510c3000d3253570e7de8c7971b949acfe3
drake-error <darwin2010p@gmail.com>
2025-10-21 22:20:30 +0530

    * pattern Q

--- array.java
+++ array.java
@@ -4,7 +4,7 @@ class array {
     { 
         for (int i=1; i<=3; i=i+1)
         {
-            for (int j=1; j<=3; j=j+1)
+            for (int j=1; j<=i; j=j+1)
             {
                 System.out.print("* ");
             }
