7c56aa5059ca18678f9d136893971bc44475457f
drake-error <darwin2010p@gmail.com>
2025-10-21 22:52:04 +0530

    Do loop file added

---
+++ q3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class q3 {
+    public static void main(String args[])
+    {
+        int count =0;
+        do {
+            System.out.println("john");
+            count = count + 1;
+        }while(count<=1);
+    }
+}
