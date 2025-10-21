139ecd59a8e4f2d20e107f2f67cf27cec0e1a9fe
drake-error <darwin2010p@gmail.com>
2025-10-21 22:43:46 +0530

    while loop q

--- array.java
+++ array.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
+import java.util.Random;
 class array {
     public static void main(String args[])
     { 
-        for (int i=1; i<=3; i=i+1)
-        {
-            for (int j=1; j<=i; j=j+1)
-            {
-                System.out.print("* ");
-            }
-            System.out.println("Nested for loop");
-        }
+       Random rand = new Random();
+       int newnum = 0;
+
+       while (newnum != 5)
+       {
+           newnum = rand.nextInt(11);
+           System.out.println(newnum);
+       }
     }
+
 }
