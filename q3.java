586eec764cbe5c485bf9179f03be8f96623e5929
drake-error <darwin2010p@gmail.com>
2025-10-28 17:23:38 +0530

    modified do-while loop q

--- q3.java
+++ q3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 class q3 {
-    public static void main(String args[])
+    public static void main(String [] args)
     {
         Scanner scan = new Scanner(System.in);
         int count =0;
