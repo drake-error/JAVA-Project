246ed80a3e16c9083007ea41b94dded6f1848407
drake-error <darwin2010p@gmail.com>
2025-11-01 20:01:38 +0530

    Fn Parameters Q

--- q2.java
+++ q2.java
@@ -1,17 +1,53 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class q2 {
-    public static void main(String args[])
+// class q2 {
+//     public static void main(String args[])
+//     {
+//         Scanner scan = new Scanner(System.in);
+//         int num1 = scan.nextInt();
+//         int num2 = scan.nextInt();
+
+//         String result = (num1 > num2) ? (num1 + " is greater") :
+//                         (num2 > num1) ? (num2 + " is greater") :
+//                         "Both numbers are equal";
+
+//         System.out.println(result);
+//         scan.close();
+//     }     
+// }
+
+// date-1/11/2025
+// Function Parameters
+
+public class q2 {
+
+    void sum(int num1, int num2)
+
     {
-        Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
+        System.out.println((num1 + num2));
+    }
 
-        String result = (num1 > num2) ? (num1 + " is greater") :
-                        (num2 > num1) ? (num2 + " is greater") :
-                        "Both numbers are equal";
+    void subtract(int num1, int num2)
+    {
+        System.out.println((num1 - num2));
+    }
 
-        System.out.println(result);
-        scan.close();
-    }     
+    void multiply(int num1, int num2)
+    {
+        System.out.println((num1 * num2));
+    }
+
+    void divide(int num1, int num2)
+    {
+        System.out.println((num1 / num2));
+    }
+
+    public static void main(String[] args) 
+    {
+        q2 obj = new q2();
+        obj.sum(10,2);
+        obj.subtract(10,2);
+        obj.multiply(10,2);
+        obj.divide(10,2);
+    }
 }
