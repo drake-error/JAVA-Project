ede7bbc47dd4ecba9309aa28511b23a895ce8159
drake-error <darwin2010p@gmail.com>
2025-11-01 19:46:08 +0530

    fn Q

--- q3.java
+++ q3.java
@@ -1,15 +1,32 @@
-import java.util.Scanner;
-import java.util.Random;
+// import java.util.Scanner;
+// import java.util.Random;
 
-class q3 {
-    public static void main(String [] args)
+// class q3 {
+//     public static void main(String [] args)
+//     {
+//         Scanner scan = new Scanner(System.in);
+//         int count =0;
+//         do {
+//             System.out.println("Enter a number>10:");
+//             count = scan.nextInt();
+//         }while(count <=10); 
+
+//     }
+// }
+
+public class q3 {
+
+    int a = 10;
+    int b = 20;
+
+    void sum()
     {
-        Scanner scan = new Scanner(System.in);
-        int count =0;
-        do {
-            System.out.println("Enter a number>10:");
-            count = scan.nextInt();
-        }while(count <=10); 
+        System.out.println(30);
+    }
 
+    public static void main(String[] args) 
+    {
+        q3 obj = new q3();
+        obj.sum();
     }
 }
