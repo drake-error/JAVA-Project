53a2bb2cb49172c6055e6722f5ab2aa3f500f3ee
drake-error <darwin2010p@gmail.com>
2025-11-01 20:18:05 +0530

    Return keywords q

--- q1.java
+++ q1.java
@@ -1,16 +1,35 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class q1 {
-    public static void main(String args[])
+// class q1 {
+//     public static void main(String args[])
+//     {
+//         Scanner scan = new Scanner(System.in);
+//         String name = scan.nextLine();
+//         int age = scan.nextInt();
+//         scan.nextLine();
+//         String address = scan.nextLine();
+//         System.out.println("My name is "+ name);
+//         System.out.println("My age is "+ age);
+//         System.out.println("I live in "+ address);
+
+//     }
+// }
+
+// Return Keywords
+// void can never return a value
+public class q1 {
+
+    int getsoap(int money)
     {
-        Scanner scan = new Scanner(System.in);
-        String name = scan.nextLine();
-        int age = scan.nextInt();
-        scan.nextLine();
-        String address = scan.nextLine();
-        System.out.println("My name is "+ name);
-        System.out.println("My age is "+ age);
-        System.out.println("I live in "+ address);
+        int soap_price = 18;
+        int rem = 20-18;
+        return rem;
+    }
 
+    public static void main(String[] args) 
+    {
+        q1 obj = new q1();
+        int remainder = obj.getsoap(20);
+        System.out.println(remainder);
     }
 }
