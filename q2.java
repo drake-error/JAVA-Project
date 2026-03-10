bba48c0b7693ca718c76d975b3cb9162147e6071
drake-error <darwin2010p@gmail.com>
2026-03-10 14:32:34 +0530

    To perfectly print as given in q e.g for score

--- q2.java
+++ q2.java
@@ -53,18 +53,34 @@
 // }
 // learnt Function Parameters and created functions for arithmetic operations
 
+// import java.util.Scanner;
+
+// public class q2 {
+//     public static void main(String argd[])
+//     {
+//         Scanner scan = new Scanner(System.in);
+//         int a = scan.nextInt();
+//         int b = scan.nextInt();
+//         int c = scan.nextInt();
+//         int d = a*b*c;
+//         int e = a+b+c;
+//         System.out.print(d/e);
+
+//     }
+// }
+
 import java.util.Scanner;
 
-public class q2 {
-    public static void main(String argd[])
+class q2 {
+    public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        int d = a*b*c;
-        int e = a+b+c;
-        System.out.print(d/e);
-
+        String name = scan.nextLine();
+        double score = scan.nextDouble();
+        scan.nextLine();
+        String department = scan.nextLine();
+        System.out.println("My Name is "+name);
+        System.out.println("My Score is "+score/10 +"/10");
+        System.out.print("My department is "+department);
     }
 }
