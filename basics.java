7fd25b01257e1fc005b29415e489b2bd38976706
drake-error <darwin2010p@gmail.com>
2026-03-10 12:21:39 +0530

    JAVA FROM SCRATCH

---
+++ basics.java
@@ -0,0 +1,24 @@
+// class basics {
+//     public static void main(String args[])
+//     {
+//         int a=10;
+//         int b=20;
+//         int Result=a+b;
+//         System.out.print(Result);
+//     }
+// }
+
+import java.lang.System;
+import java.util.Scanner;
+
+class basics{
+    public static void main(String args[])
+    {
+        Scanner number = new Scanner(System.in);
+        int a = number.nextInt();
+        int b = number.nextInt();
+        int c = a+b;
+
+        System.out.println(c);
+    }
+}
