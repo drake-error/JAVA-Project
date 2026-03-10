b3166d2cf2f7da518028ebff2946a2733cf80e3a
drake-error <darwin2010p@gmail.com>
2026-03-10 13:30:43 +0530

    Follow the question , just mention no.of variables i.e. in q

--- q2.java
+++ q2.java
@@ -1,54 +1,70 @@
-// import java.util.Scanner;
+// // import java.util.Scanner;
 
-// class q2 {
-//     public static void main(String args[])
+// // class q2 {
+// //     public static void main(String args[])
+// //     {
+// //         Scanner scan = new Scanner(System.in);
+// //         int num1 = scan.nextInt();
+// //         int num2 = scan.nextInt();
+
+// //         String result = (num1 > num2) ? (num1 + " is greater") :
+// //                         (num2 > num1) ? (num2 + " is greater") :
+// //                         "Both numbers are equal";
+
+// //         System.out.println(result);
+// //         scan.close();
+// //     }     
+// // }
+
+// // date-1/11/2025
+// // Function Parameters
+
+// public class q2 {
+
+//     void sum(int num1, int num2)
+
 //     {
-//         Scanner scan = new Scanner(System.in);
-//         int num1 = scan.nextInt();
-//         int num2 = scan.nextInt();
+//         System.out.println((num1 + num2));
+//     }
 
-//         String result = (num1 > num2) ? (num1 + " is greater") :
-//                         (num2 > num1) ? (num2 + " is greater") :
-//                         "Both numbers are equal";
+//     void subtract(int num1, int num2)
+//     {
+//         System.out.println((num1 - num2));
+//     }
 
-//         System.out.println(result);
-//         scan.close();
-//     }     
+//     void multiply(int num1, int num2)
+//     {
+//         System.out.println((num1 * num2));
+//     }
+
+//     void divide(int num1, int num2)
+//     {
+//         System.out.println((num1 / num2));
+//     }
+
+//     public static void main(String[] args) 
+//     {
+//         q2 obj = new q2();
+//         obj.sum(10,2);
+//         obj.subtract(10,2);
+//         obj.multiply(10,2);
+//         obj.divide(10,2);
+//     }
 // }
+// learnt Function Parameters and created functions for arithmetic operations
 
-// date-1/11/2025
-// Function Parameters
+import java.util.Scanner;
 
 public class q2 {
-
-    void sum(int num1, int num2)
-
+    public static void main(String argd[])
     {
-        System.out.println((num1 + num2));
-    }
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = a*b*c;
+        int e = a+b+c;
+        System.out.print(d/e);
 
-    void subtract(int num1, int num2)
-    {
-        System.out.println((num1 - num2));
-    }
-
-    void multiply(int num1, int num2)
-    {
-        System.out.println((num1 * num2));
-    }
-
-    void divide(int num1, int num2)
-    {
-        System.out.println((num1 / num2));
-    }
-
-    public static void main(String[] args) 
-    {
-        q2 obj = new q2();
-        obj.sum(10,2);
-        obj.subtract(10,2);
-        obj.multiply(10,2);
-        obj.divide(10,2);
     }
 }
-// learnt Function Parameters and created functions for arithmetic operations
