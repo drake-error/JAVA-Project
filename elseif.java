2275afe6d1f2a0693ce49d75ed4f2b716e45fd62
drake-error <darwin2010p@gmail.com>
2026-03-14 23:34:22 +0530

    Understanding if-else

--- elseif.java
+++ elseif.java
@@ -1,26 +1,43 @@
 import java.util.Scanner;
 
-public class elseif{
+// public class elseif{
+//     public static void main(String args[])
+//     {
+//        Scanner scan = new Scanner(System.in);
+//        int s1 = scan.nextInt();
+//        int s2 = scan.nextInt();
+//        int s3 = scan.nextInt();
+//        int s4 = scan.nextInt();
+//        int s5 = scan.nextInt();
+
+//        int avg = (s1+s2+s3+s4+s5)/5;
+
+//          if(avg<35)
+//          {
+//           System.out.println("Additional classes are required");
+//          }
+//          else
+//         {
+//             System.out.println("No need of additional classes");
+//         }
+        
+// }   
+
+// }
+
+public class elseif {
     public static void main(String args[])
     {
-       Scanner scan = new Scanner(System.in);
-       int s1 = scan.nextInt();
-       int s2 = scan.nextInt();
-       int s3 = scan.nextInt();
-       int s4 = scan.nextInt();
-       int s5 = scan.nextInt();
+        Scanner scan = new Scanner(System.in);
+        String RCB = "loose";
 
-       int avg = (s1+s2+s3+s4+s5)/5;
-
-         if(avg<35)
-         {
-          System.out.println("Additional classes are required");
-         }
-         else
+        if(RCB.equals("win"))
         {
-            System.out.println("No need of additional classes");
+            System.out.print("EE SALA CUP NAMDHEY");
         }
-        
-}   
-
+        else
+        {
+            System.out.print("Cup illa");
+        }
+    }
 }
