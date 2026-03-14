683084d0f76453b2d10c248b0b4eecd4ee396038
drake-error <darwin2010p@gmail.com>
2026-03-14 23:22:22 +0530

    NUMBER COMPARISON

--- demo.java
+++ demo.java
@@ -1,28 +1,48 @@
-import java.util.Scanner;
-class demo {
-    public static void main(String[] args) 
-    {
-        // Cricket or Football = Play
-        // true or true = true (play)
-        // true or false = true (play)
-        // false or true = true (play)
-        // false or false = false (do not play)
+// import java.util.Scanner;
+// class demo {
+//     public static void main(String[] args) 
+//     {
+//         // Cricket or Football = Play
+//         // true or true = true (play)
+//         // true or false = true (play)
+//         // false or true = true (play)
+//         // false or false = false (do not play)
 
-        boolean Cricket = false;
-        boolean Football = false;
+//         boolean Cricket = false;
+//         boolean Football = false;
 
-        if(Cricket || Football)
-        {
-            System.out.println("Play");
-        }
-        else
-        {
-            System.out.println("Do not Play");
-        }
+//         if(Cricket || Football)
+//         {
+//             System.out.println("Play");
+//         }
+//         else
+//         {
+//             System.out.println("Do not Play");
+//         }
         
         
-    }
+//     }
     
+// }
+
+import java.util.Scanner;
+
+class demo {
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+if(num1==num2)
+{
+    System.out.print("Number 1 is Equal to Number 2");
+}
+else
+{
+    System.out.print("Number 1 is not Equal to Number 2");
+}
+    }
 }
 
       
