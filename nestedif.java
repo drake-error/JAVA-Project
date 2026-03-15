3c8489bb6602db637328ba96da6b807a93b6c76b
drake-error <darwin2010p@gmail.com>
2026-03-15 15:27:14 +0530

    UNDERSTANDING else-if logic

--- nestedif.java
+++ nestedif.java
@@ -1,23 +1,46 @@
-class nestedif {
+// class nestedif {
+//     public static void main(String args[])
+//     {
+//         boolean kfc = true;
+//         boolean chicken = true;
+//         boolean pepsi = true;
+
+//         if(kfc==true)
+//         {
+//             System.out.println("Enter into KFC");
+
+//            if(chicken==true)
+//            {
+//             System.out.println("Eating chciken");
+//            if(pepsi==true)
+//           {
+//             System.out.println("Drinking pepsi");
+//           }
+//            }
+//         }
+
+//     }
+// }
+
+import java.util.Scanner;
+
+public class nestedif{
     public static void main(String args[])
     {
-        boolean kfc = true;
-        boolean chicken = true;
-        boolean pepsi = true;
+        Scanner scan = new Scanner(System.in);
+        int score = scan.nextInt();
 
-        if(kfc==true)
+        if(score<50)
         {
-            System.out.println("Enter into KFC");
-
-           if(chicken==true)
-           {
-            System.out.println("Eating chciken");
-           if(pepsi==true)
-          {
-            System.out.println("Drinking pepsi");
-          }
-           }
+            System.out.print("YOU NEED TO IMPROVE");
         }
-
+        else if(score>=50 && score<=70)
+        {
+            System.out.print("Good Job!");
+        }
+        else if(score>70)
+        {
+            System.out.print("Excellent performance");
+        }
     }
 }
