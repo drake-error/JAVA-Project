c1c21de158cff7cbbbe4c221da142d4c3cadbff0
drake-error <darwin2010p@gmail.com>
2026-03-15 15:43:03 +0530

    CHECK THE ERROR IN THIS CODE - FOR WEIRD U WROTE WERID

--- hi.java
+++ hi.java
@@ -1,22 +1,51 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class hi {
+// class hi {
+//     public static void main(String args[])
+//     {
+//         // Create a Variable called num
+//         // Find out whether num is divisible by 2 or not
+
+//         Scanner scan = new Scanner(System.in);
+//         int num = scan.nextInt();
+//         if(num%2==0)
+//         {
+//             System.out.print("Even");
+//         }
+//         else
+//         {
+//             System.out.print("Odd");
+//         }
+        
+//     }
+
+// }
+
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class hi{
     public static void main(String args[])
     {
-        // Create a Variable called num
-        // Find out whether num is divisible by 2 or not
-
         Scanner scan = new Scanner(System.in);
-        int num = scan.nextInt();
-        if(num%2==0)
-        {
-            System.out.print("Even");
-        }
-        else
-        {
-            System.out.print("Odd");
-        }
+        int n = scan.nextInt();
         
+        if(n%2==1)
+        {
+            System.out.print("Weird");
+        }
+        else if(n%2==0 && n>=2 && n<=5)
+        {
+            System.out.print("Not Werid");
+        }
+        else if(n%2==0 && n>=6 && n<=20)
+        {
+            System.out.print("Weird");
+        }
+        else if(n%2==0 && n>20)
+        {
+            System.out.print("Not Weird");
+        }
     }
-
 }
