4738394f468a99e8fa98881301a04917626d920a
drake-error <darwin2010p@gmail.com>
2026-03-15 15:27:20 +0530

    UNDERSTANDING else-if logic

--- nestedif.java
+++ nestedif.java
@@ -32,15 +32,15 @@ public class nestedif{
 
         if(score<50)
         {
-            System.out.print("YOU NEED TO IMPROVE");
+            System.out.print("YOU NEED TO IMPROVE"); // input is 50 hence false
         }
         else if(score>=50 && score<=70)
         {
-            System.out.print("Good Job!");
+            System.out.print("Good Job!");// prints good job bcz input is 50 and condition mentioned for score>=50 matches with it 
         }
         else if(score>70)
         {
-            System.out.print("Excellent performance");
+            System.out.print("Excellent performance"); // never executes in else-if
         }
     }
 }
