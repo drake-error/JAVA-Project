b187ef6df4b2517b01b40c7301d0c5e97cf77d91
drake-error <darwin2010p@gmail.com>
2026-03-16 20:12:43 +0530

    CLEARED THE ERROR

--- nestedif.java
+++ nestedif.java
@@ -22,25 +22,48 @@
 //     }
 // }
 
+// import java.util.Scanner;
+
+// public class nestedif{
+//     public static void main(String args[])
+//     {
+//         Scanner scan = new Scanner(System.in);
+//         int score = scan.nextInt();
+
+//         if(score<50)
+//         {
+//             System.out.print("YOU NEED TO IMPROVE"); // input is 50 hence false
+//         }
+//         else if(score>=50 && score<=70)
+//         {
+//             System.out.print("Good Job!");// prints good job bcz input is 50 and condition mentioned for score>=50 matches with it 
+//         }
+//         else if(score>70)
+//         {
+//             System.out.print("Excellent performance"); // never executes in else-if
+//         }
+//     }
+// }
 import java.util.Scanner;
 
-public class nestedif{
+public class nestedif {
     public static void main(String args[])
     {
         Scanner scan = new Scanner(System.in);
-        int score = scan.nextInt();
+        int s1 = scan.nextInt();
+        int s2= scan.nextInt();
+        int s3= scan.nextInt();
+        int s4= scan.nextInt();
+        int s5= scan.nextInt();
+        int average = (1+s2+s3+s4+s5)/5;
 
-        if(score<50)
+        if(average<35)
         {
-            System.out.print("YOU NEED TO IMPROVE"); // input is 50 hence false
+            System.out.print("Additional Classes required");
         }
-        else if(score>=50 && score<=70)
-        {
-            System.out.print("Good Job!");// prints good job bcz input is 50 and condition mentioned for score>=50 matches with it 
+        else{
+            System.out.print("You are good to go");
         }
-        else if(score>70)
-        {
-            System.out.print("Excellent performance"); // never executes in else-if
-        }
+
     }
 }
