fd952aa0f39fdc18866df50eb59614a9ecf10789
drake-error <darwin2010p@gmail.com>
2026-03-17 00:24:36 +0530

    Ternary Operator LOGIC

---
+++ ternary.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ternary {
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+     String result=a>b? "A is greater":"B is Greater";
+
+        System.out.print(result);
+    }
+}
