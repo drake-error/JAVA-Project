1d18e6e90f33ba2edd9175724d837fd08fb665d5
drake-error <darwin2010p@gmail.com>
2026-03-20 18:39:22 +0530

    printing *** in same row for 3 columns

--- nestedloop.java
+++ nestedloop.java
@@ -1,11 +1,25 @@
-class nestedloop {
+// class nestedloop {
+//     public static void main(String[] args)
+//     {
+//         for(int i=1;i<=2;i=i+1)
+//         {
+//             for(int count=1;count<=2;count = count +1)
+//             {
+//                 System.out.println("Darwin");
+//             }
+//         }
+//     }
+//
+
+class nestedloop{
     public static void main(String[] args)
     {
-        for(int i=1;i<=2;i=i+1)
+        for(int i=1;i<=3;i=i+1)
         {
-            for(int count=1;count<=2;count = count +1)
+            System.out.println("");
+            for(int j=1;j<=3;j=j+1)
             {
-                System.out.println("Darwin");
+                System.out.print("*");
             }
         }
     }
