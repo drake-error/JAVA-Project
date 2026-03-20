12483d7d652f9910c42f55fea3e382bf0e5b8c61
drake-error <darwin2010p@gmail.com>
2026-03-20 21:17:31 +0530

    printing *,**,*** in 3 diff row--ACTUAL ANSWER WITH LOGIC

--- nestedloop.java
+++ nestedloop.java
@@ -14,9 +14,15 @@
 class nestedloop{
     public static void main(String[] args)
     {
-        for(int j=1;j<=3;j=j+1)
+        for(int j=1;j<=3;j=j+1) //STEP 0: LOOP j: J=1 , cond gets TRUE and goes into BRANCH LOOP
         {
-            for(int i=1;i<=j;i=i+1)
+            for(int i=1;i<=j;i=i+1) //LOOP I: STEP 1:   i=1 , 1<=1(TRUE) prints *
+            //STEP 2: i=1+1=2 , 2<=1(j) COND(FALSE) LOOP BREAKS GOES TO LOOP j
+            //IN LOOP J Goes for j=j+1 i.e. j=1+1=2 (Latest value of J is 1 acc to STEP 0) ,2<=3(TRUE)
+            //STEP 3: i=1 , 1<=2(TRUE) prints *
+            //STEP 4: i=1+1=2 , 2<=2(TRUE) prints ** (IF PRINTLN WAS THERE THEN IT WOULD HV PRINTED * 
+                                                                                                //  * )
+                                                                                                //AGAIN LOOP BREAKS AND FOLLOWS STEP 0.
             {
                 System.out.print("*");
             }
