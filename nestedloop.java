fd3073977e6fe20728fa6f7c7e84aab5aa4e8d01
drake-error <darwin2010p@gmail.com>
2026-03-20 21:04:42 +0530

    printing *,**,*** in 3 diff row ---MY TRY

--- nestedloop.java
+++ nestedloop.java
@@ -14,13 +14,17 @@
 class nestedloop{
     public static void main(String[] args)
     {
-        for(int i=1;i<=3;i=i+1)
+        for(int i=1;i<=1;i=i+1)
         {
-            System.out.println("");
-            for(int j=1;j<=3;j=j+1)
+            System.out.println("*");
+            for(int j=1;j<=1;j=j+1)
             {
-                System.out.print("*");
+                System.out.println("**");
+            for(int k=1;k<=1;k=k+1)
+            {
+                System.out.println("***");
             }
         }
     }
+}
 }
